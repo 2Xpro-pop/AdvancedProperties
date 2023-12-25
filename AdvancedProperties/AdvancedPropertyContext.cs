8bcada4c1383041f5071bcd61a6f5918964c6417
2Xpro-pop <bay.asanov@mail.ru>
2023-12-26 01:38:02 +0600

    added basics AdvancedPropertyContext

---
+++ AdvancedProperties/AdvancedPropertyContext.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommunityToolkit.Diagnostics;
+
+namespace AdvancedProperties;
+public abstract class AdvancedPropertyContext
+{
+    public AdvancedProperty Property
+    {
+        get;
+    }
+
+    protected AdvancedPropertyContext(AdvancedProperty advancedProperty)
+    {
+        Property = advancedProperty;
+    }
+}
+
+public class AdvancedPropertyContext<TOwner, TValue> : AdvancedPropertyContext
+{
+    public AdvancedPropertyContext(AdvancedProperty<TOwner, TValue> advancedProperty, TOwner owner) : base(advancedProperty)
+    {
+        Guard.IsNotNull(advancedProperty, nameof(advancedProperty));
+        Guard.IsNotNull(owner, nameof(owner));
+
+        Property = advancedProperty;
+        Owner = owner;
+
+        if (advancedProperty.DefaultValue is TValue defaultValue)
+        {
+            Value = defaultValue;
+        }
+        else if (advancedProperty.CreateDefaultValue is AdvancedProperty.CreateDefaultValueDelegate<TOwner, TValue> defaultValueDelegate)
+        {
+            Value = defaultValueDelegate(owner);
+        }
+        else
+        {
+            Value = default!;
+        }
+
+        Context = Value ?? AdvancedProperty.UnsetValue;
+        CurrentPriority = AdvancedSetterPriority.Default;
+    }
+
+    public new AdvancedProperty<TOwner, TValue> Property
+    {
+        get;
+    }
+
+    public TOwner Owner
+    {
+        get;
+    }
+
+    /// <summary>
+    /// Contains value, binding, or resource reference the property.
+    /// </summary>
+    public object Context
+    {
+        get; protected set;
+    }
+
+    public TValue Value
+    {
+        get; protected set;
+    }
+
+    public AdvancedSetterPriority CurrentPriority
+    {
+        get; protected set;
+    }
+}
---
+++ AdvancedProperties/AdvancedSetterPriority.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdvancedProperties;
+
+/// <summary>
+/// Represents priority levels for setting values.
+/// A value can only be set if the priority level of the action is lower or equal to the current priority level.
+/// For example, a value with 'Direct' priority can always be set, 
+/// while a value with 'Default' priority can be replaced by any other priority level.
+/// </summary>
+public enum AdvancedSetterPriority
+{
+    Direct, // Highest priority
+    Binding,
+    Init,
+    Default, // Lowest priority
+}
